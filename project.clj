fc348b15f71d94c26c8b9367aefe01f46a3b49d9
Michael Locher <cmbntr@gmail.com>
2012-02-24 17:41:34 +0100

    Add project URL.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject cmbntr/cmbntr.org "0.0.1-SNAPSHOT"
   :description "org-mode outlining utilities"
+  :url "https://github.com/cmbntr/cmbntr.org"
   :dependencies [[org.clojure/clojure "1.3.0"]])
