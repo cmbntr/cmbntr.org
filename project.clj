66f9f58445512138f828b1af4dd0c04b19b18f4f
Michael Locher <cmbntr@gmail.com>
2012-02-27 23:42:02 +0100

    Version 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cmbntr/cmbntr.org "0.0.1-SNAPSHOT"
+(defproject cmbntr/cmbntr.org "0.0.1"
   :description "org-mode outlining utilities"
   :url "https://github.com/cmbntr/cmbntr.org"
   :dependencies [[org.clojure/clojure "1.3.0"]])
